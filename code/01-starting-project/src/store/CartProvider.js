79c3847e832dbdcd58378533c0828aead483f44d
Sai Krishna Kalakonda <saikrishna.kalakonda@ibm.com>
2022-09-22 02:49:52 -0400

    148 making items removable

--- code/01-starting-project/src/store/CartProvider.js
+++ code/01-starting-project/src/store/CartProvider.js
@@ -7,7 +7,9 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+
   if (action.type === "ADD") {
+
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex(
@@ -33,6 +35,24 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   } else if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    if(existingCartItem.amount === 1) {
+        updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+        const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1};
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+    }
   }
   return defaultCartState;
 };
